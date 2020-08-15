c9f2cffe1c58b21e63204047eeb82622c69122a3
Jordan Scales <scalesjordan@gmail.com>
2020-08-15 18:39:21 -0400

    req.body doesn't work I guess

--- api/post.js
+++ api/post.js
@@ -1,41 +1,50 @@
+const querystring = require("querystring");
 const contentful = require("contentful-management");
 const { v4: uuidv4 } = require("uuid");
-const strftime = require('strftime').timezone('-0400')
+const strftime = require("strftime").timezone("-0400");
 
 module.exports = (req, res) => {
+  let incoming = "";
+  req.on("data", (chunk) => {
+    incoming += chunk.toString();
+  });
+  req.on("end", () => {
+    const { token, space, body } = querystring.parse(incoming);
+
     const client = contentful.createClient({
-        accessToken: req.body.token,
-    })
+      accessToken: token,
+    });
 
-    const id = 'sms-' + uuidv4()
-    const [title, ...lines] = req.body.text.split(/(?:\r?\n){2,}/)
+    const id = "sms-" + uuidv4();
+    const [title, ...lines] = body.split(/(?:\r?\n){2,}/);
 
-    client.getSpace(req.body.space)
-        .then((space) => {
-            space.createEntryWithId(id, 'Entry', {
-                fields: {
-                    title: {
-                        'en-US': title,
-                    },
-                    date: strftime('%FT%H:%M-04:00', new Date()),
-                    body: {
-                        'en-US': lines.join('\n\n'),
-                    },
-                    gallery: [],
-                    score: 0,
-                }
-            })
+    client
+      .getSpace(space)
+      .then((space) =>
+        space.createEntryWithId("post", id, {
+          fields: {
+            title: {
+              "en-US": title,
+            },
+            date: {
+              "en-US": strftime("%FT%H:%M-04:00", new Date()),
+            },
+            body: {
+              "en-US": lines.join("\n\n"),
+            },
+            gallery: {
+              "en-US": [],
+            },
+            score: {
+              "en-US": 0,
+            },
+          },
         })
-        .then((entry) => entry.publish())
-        .then((entry) => {
-            res.json({
-                'message': `Entry ${id} published`,
-            })
-        })
-        .catch((err) => {
-            res.statusCode = 404;
-            res.json({
-                err: err
-            })
-        })
-}
+      )
+      .then((entry) => entry.publish())
+      .then((entry) => {
+        res.send(id);
+      })
+      .catch(console.error);
+  });
+};
